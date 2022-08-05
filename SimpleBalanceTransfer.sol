e78e9383ca3ad25e1a1720fd4078e9fff0426a24
Sourin <98945276+heysourin@users.noreply.github.com>
2022-08-05 11:39:42 +0530

    Simple Balance Transfer Smart Contract
    Balance: acc1--> contract --> acc2

---
+++ SimpleBalanceTransfer.sol
@@ -0,0 +1,27 @@
+//Balance: acc1--> contract --> acc2
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Wallet {
+
+    address payable public owner;
+
+    constructor(){
+        owner = payable(msg.sender);
+    }
+
+//deposit ( ) - To deposit ether to contract balance.
+    function deposit() payable public{
+    }
+
+    function sendEther(address payable _to, uint _amt)  public payable{
+        require(msg.sender == owner, "sender is not allowed");
+        _to.transfer(_amt);
+    }
+
+    function balanceOf() public view returns(uint){
+        return address(this).balance;
+    }
+
+}
