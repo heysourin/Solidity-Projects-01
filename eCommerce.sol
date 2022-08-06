c82f282360036cd66ef0c0b80d12784a48825256
Sourin <98945276+heysourin@users.noreply.github.com>
2022-08-06 11:34:46 +0530

    Update eCommerce.sol

--- eCommerce.sol
+++ eCommerce.sol
@@ -16,6 +16,13 @@ contract ecommerce{
     uint count = 1;
     Product[] public products;
     address payable public manager;
+
+    bool destroyed = false;
+
+    modifier isNotDestroyed{
+        require(!destroyed, "Contract does not exist");
+        _;
+    }
     constructor(){
         manager = payable(msg.sender);
     }
@@ -24,7 +31,7 @@ contract ecommerce{
     event bought(uint productId, address buyer);
     event delivered(uint productId);
 
-    function registerproduc(string memory _title, string memory _descrp, uint _price) public{
+    function registerproduc(string memory _title, string memory _descrp, uint _price) public isNotDestroyed{
         require(_price > 0, "Should be greater than zero");
 
         Product memory tempProduct;
@@ -39,7 +46,7 @@ contract ecommerce{
         emit registered(_title, tempProduct.serialNum, msg.sender);
     } 
 
-    function buy(uint _serialNum) payable public{
+    function buy(uint _serialNum) payable public isNotDestroyed{
         require(products[_serialNum-1].price == msg.value,"Pay the exact price");
         require(products[_serialNum-1].seller != msg.sender,"Seller can't buy");
 
@@ -47,16 +54,26 @@ contract ecommerce{
         emit bought(_serialNum,msg.sender);
     }
 
-    function delivery(uint _serialNum) public{
+    function delivery(uint _serialNum) public isNotDestroyed{
         require(products[_serialNum-1].buyer == msg.sender, "Only buyer can confirm this");
         products[_serialNum-1].isDelivered = true;
         products[_serialNum-1].seller.transfer(products[_serialNum-1].price);
         emit delivered(_serialNum);
     }
 
-    function destroy() public{
-        require(msg.sender == manager, "Only manager can call this function");
-        selfdestruct(manager);// contract will be destroyed annd all the funds in the contract will be transfered to the manager
-        //Why: to be safe from hacker
+    // function destroy() public{
+    //     require(msg.sender == manager, "Only manager can call this function");
+    //     selfdestruct(manager);// contract will be destroyed annd all the funds in the contract will be transfered to the manager
+    //     //Why: to be safe from hacker
+    // }
+
+    function destroy() public isNotDestroyed{
+        require(manager == msg.sender);
+        manager.transfer(address(this).balance);
+        destroyed = true;
     }
+
+    fallback() payable external{
+        payable(msg.sender).transfer(msg.value);
+    }    
 }
