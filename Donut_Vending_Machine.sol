c57d5926395b935240b1449cc646ee38f85bc73c
Sourin <98945276+heysourin@users.noreply.github.com>
2022-09-07 17:44:21 +0530

    Payments updated
    A smart contract of donut vending machine. Where only owner can reset the stock, owner can't make a purchase.
    There is a function in which u set the amount of donuts u wanna buy and enter the value in the dashboard.

--- Donut_Vending_Machine.sol
+++ Donut_Vending_Machine.sol
@@ -1,29 +1,34 @@
 //SPDX-License-Identifier:MIT
 pragma solidity >=0.5.0 <=0.9.0;
 
-contract VendingMachine{
-    address public owner;  //setting owner
-    mapping(address => uint) public donutNumbers;
-    
-    constructor(){
-        owner = msg.sender;//msg: gloobal variable, sender is a property of it
-        donutNumbers[address(this)] = 100; //address of the deployed contract,not of wallet
+contract vendingMachine2{
+    address payable public owner;
+    mapping(address => uint) public donutAmount;
+
+    constructor(uint _amt){
+        owner = payable(msg.sender);
+        donutAmount[address(this)] = _amt;
     }
 
-    function getdonutNumber() public view returns(uint){
-        return donutNumbers[address(this)];
+    function seeAmount() public view returns(uint){
+        return donutAmount[address(this)];
     }
 
-    function purchase(uint amount) public payable{
-        require(msg.value >= amount * 2 ether, "Must pay atleast 2 ethers per donut");
-        require(msg.sender != owner, "Owner can't buy");//owner allow deployer account to buy donuts
-        require(donutNumbers[address(this)] >= amount, "Not enough dounuts availble");//must have more donuts than the order
-        donutNumbers[address(this)] -= amount;
-        donutNumbers[msg.sender] += amount;
+    function buy(uint _amt) public payable{
+        require(msg.value >= (_amt * 1 ether),"Cost: 1 ether per pc.");
+        require(msg.sender != owner, "Owner can't make a purchase");
+        require(donutAmount[address(this)] > _amt, "Not so much available");
+
+        donutAmount[address(this)] -= 1;
+        donutAmount[msg.sender] += 1;
     }
 
-    function restock(uint amount) public {
-        require(msg.sender == owner, "Only owner can restock the machine");
-        donutNumbers[address(this)] += amount;
+    function restock(uint _amt) public{
+        require(msg.sender == owner, "Only owner can restock");
+        donutAmount[address(this)] += _amt;
     }
+
+    function withdrawBalance() public payable{
+        require(msg.sender == owner, "Only owner can withdraw");
+        owner.transfer(address(this).balance);    }
 }
