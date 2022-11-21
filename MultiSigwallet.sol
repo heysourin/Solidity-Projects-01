e488abf06b15b1534f4fee579606ab8f7e514b3e
Sourin <98945276+heysourin@users.noreply.github.com>
2022-11-21 23:43:33 +0530

    Update MultiSigwallet.sol

--- MultiSigwallet.sol
+++ MultiSigwallet.sol
@@ -7,12 +7,37 @@ contract MultisigWallet {
         address indexed owner,
         uint256 indexed transactionId,
         address indexed transactionTo,
-        uint value,
+        uint256 value,
         bytes data
     );
-    event transcactionConfirmation(address indexed owner, uint indexed transactionId);
-    event revokeConfirmation(address indexed owner, uint indexed transactionId);
-    event executeTranscaction(address indexed owner, uint indexed transactionId);
+    event transcactionConfirmation(
+        address indexed owner,
+        uint256 indexed transactionId
+    );
+    event revokeConfirmation(
+        address indexed owner,
+        uint256 indexed transactionId
+    );
+    event executeTranscaction(
+        address indexed owner,
+        uint256 indexed transactionId
+    );
+
+    address[] owners;
+    mapping(address => bool) public isOwner;
+    uint256 public numOfConfirmationRequired;
+
+    struct Transaction {
+        address to;
+        uint256 value;
+        bytes data;
+        bool executed;
+        uint256 numConfirms;
+    }
+
+    mapping(uint256 => mapping(address => bool)) public isConfirmed;
+
+    Transaction[] transactions;
 
     
 }
