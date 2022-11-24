81614248951c6e10a19d9f487a45a500d1add8da
Sourin <98945276+heysourin@users.noreply.github.com>
2022-11-24 23:36:22 +0530

    Update DynamicNFT.sol

--- DynamicNFT.sol
+++ DynamicNFT.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract dNFT is ERC721Enumerable, Ownable {
-    using Strings for uint256;
+    using Strings for uint256;//It will be used to make tokenId -> string in later part
 
     string public baseURI;
     string public baseExtension = ".json";
