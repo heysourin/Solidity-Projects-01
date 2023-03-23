c005d272a7c96d7ae819aece4e1e9e885baf53f3
Sourin <98945276+heysourin@users.noreply.github.com>
2023-03-24 00:53:13 +0530

    Create Airdrop.sol

---
+++ NFTs/Airdrop/Airdrop.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+contract NFTAirdrop {
+    function bulkAirdropNFT(
+        IERC721 _token,
+        address[] calldata _to,
+        uint256[] calldata _id
+    ) public {
+        require(
+            _to.length == _id.length,
+            "Token ID's don't match the number of receivers"
+        );
+        for (uint256 i = 0; i < _to.length; i++) {
+            _token.safeTransferFrom(msg.sender, _to[i], _id[i]);
+        }
+    }
+}
