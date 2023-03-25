4718a13e17d24773548088876a1ab54357fa8a22
Sourin <98945276+heysourin@users.noreply.github.com>
2023-03-25 23:04:37 +0530

    Create SimpleFlashLoan.sol

---
+++ DeFi/Flash Loans/SimpleFlashLoan.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.10;
+import "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
+import "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
+import "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
+
+contract ArbitrageFlashloan is FlashLoanSimpleReceiverBase {
+    address payable owner;
+
+    constructor(address _addressProvider)
+        FlashLoanSimpleReceiverBase(IPoolAddressesProvider(_addressProvider))
+    {
+        owner = payable(msg.sender);
+    }
+
+    /*
+@param _token adddress of USDC token
+@param _amount of USDC with 6 zeros
+*/
+    function fn_RequestFlashLoan(address _token, uint256 _amount) public {
+        address receiverAddress = address(this);
+        address asset = _token;
+        uint256 amount = _amount;
+        bytes memory params = "";
+        uint16 referralCode = 0;
+
+        POOL.flashLoanSimple(
+            receiverAddress,
+            asset,
+            amount,
+            params,
+            referralCode
+        );
+    }
+
+    function arbitrargeUSDC(address _tokenAddress, uint256 _amount)
+        private
+        returns (bool)
+    {
+        uint256 arbitraged_amount = (_amount / 10);
+        IERC20 token = IERC20(_tokenAddress);
+        return token.transfer(owner, arbitraged_amount);
+    }
+
+    function executeOperation(
+        address asset,
+        uint256 amount,
+        uint256 premium,
+        address initiator,
+        bytes calldata params
+    ) external override returns (bool) {
+        //
+        // This contract now has the funds requested.
+        // Your logic goeas here.
+        //
+        bool status = arbitrargeUSDC(asset, amount);
+
+        // At the end of your logic above, this contract owes
+        // the flashloaned amount + premiums.
+        // Therefore ensure your contract has enough to repay
+        // these amounts.
+
+        // Approve the Pool contract allowance to *pull* the owed amount
+        uint256 totalAmount = amount + premium;
+        IERC20(asset).approve(address(POOL), totalAmount);
+
+        return status;
+    }
+
+    receive() external payable {}
+}
