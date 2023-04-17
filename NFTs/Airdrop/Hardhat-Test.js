aef4069e3d66b713f9fd413ba002e7915bec448a
Sourin <98945276+heysourin@users.noreply.github.com>
2023-04-18 01:28:59 +0530

    Update Hardhat-Test.js

--- NFTs/Airdrop/Hardhat-Test.js
+++ NFTs/Airdrop/Hardhat-Test.js
@@ -1,8 +1,3 @@
-const {
-  time,
-  loadFixture,
-} = require("@nomicfoundation/hardhat-network-helpers");
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
@@ -11,7 +6,7 @@ describe("Lock", function () {
   let nftContract;
   let airdropContract;
   beforeEach(async () => {
-    [owner, user1, user2, user3] = await ethers.getsigners();
+    [owner, user1, user2, user3] = await ethers.getSigners();
 
     const NFTContract = await ethers.getContractFactory("PixelNFTs");
     nftContract = await NFTContract.deploy();
@@ -21,6 +16,43 @@ describe("Lock", function () {
   });
 
   describe("It will mint Tokens in the erc721 smart contract", function () {
-    it("Should mint a single NFT", async () => {});
+    it("Should mint a single NFT", async () => {
+      await nftContract.safeMint();
+
+      expect(await nftContract.balanceOf(owner.getAddress())).to.equal("1");
+    });
+
+    it("Should mint NFTs in bulk", async () => {
+      await nftContract.bulkMint(10);
+
+      expect(await nftContract.balanceOf(owner.getAddress())).to.equal("10");
+    });
+  });
+
+  describe("It will test Airdrop contract", function () {
+    it("Should test Airdrop contract and return the correct owner of the NFTs", async () => {
+      await nftContract.bulkMint(10);
+
+      await nftContract.setApprovalForAll(airdropContract.address, true);
+
+      const nftContractAddress = await nftContract.address;
+
+      const user1Address = await user1.getAddress();
+      const user2Address = await user2.getAddress();
+      const user3Address = await user3.getAddress();
+
+      const userArray = [user1Address, user2Address, user3Address];
+
+      const nftId = ["2", "3", "5"];
+      await airdropContract.bulkAirdropNFT(
+        nftContractAddress,
+        userArray,
+        nftId
+      );
+
+      expect(await nftContract.ownerOf(2)).to.equal(user1Address);
+      expect(await nftContract.ownerOf(3)).to.equal(user2Address);
+      expect(await nftContract.ownerOf(5)).to.equal(user3Address);
+    });
   });
 });
